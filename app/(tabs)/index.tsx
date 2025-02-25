c1a043fbb6fb840b5f3b0b55d1ebdffc23bb539c
Tim Schupp <tim@timschupp.de>
2025-02-25 11:15:47 +0100

    updated repo configuration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,53 +4,35 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import Login from '@/main_frontend/src/components/views/Login';
+import { Provider } from 'react-redux';
+import store from '@/main_frontend/src/app/store';
+
+import {
+  CustomThemeProvider,
+  GlobalStyles,
+  Card,
+  Button,
+} from '@a-little-world/little-world-design-system';
+
+import styled from 'styled-components/native';
+import { ThemeProvider } from 'styled-components/native';
+
+export const StyledCard = styled(Card)`
+  position: relative;
+  max-width: 500px;
+  align-self: flex-start;
+  flex: 1;
+`;
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <Provider store={store}>
+      <CustomThemeProvider>
+        <GlobalStyles />
+        <Login />
+      </CustomThemeProvider>
+    </Provider>
   );
 }
 
