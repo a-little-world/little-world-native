118867e88577716badc8ebbb1f87abe7d00622ac
Tim Schupp <tim@timschupp.de>
2025-03-12 12:11:51 +0100

    updated login page

--- components/views/Login.jsx
+++ components/views/Login.jsx
@@ -146,6 +146,7 @@ const Login = () => {
             label={t('login.email_label')}
             error={t(errors?.email?.message)}
             placeholder={t('login.email_placeholder')}
+            toolTipText="test"
             type="email"
         />
         <TextInput
