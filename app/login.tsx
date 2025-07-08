03028d083ff91cb5e616ffbc3e34f4363a30ee89
Tim Schupp <tim@timschupp.de>
2025-07-08 17:40:36 +0200

    Test code sharing

--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Login from "@/src/components/views/Login";
 import FormLayout from "@/src/components/blocks/Layout/FormLayout";
+import { Button as WebButton, CustomThemeProvider } from "@a-little-world/little-world-design-system";
 
 export default function LoginPage() {
   return (
     <FormLayout>
+      <CustomThemeProvider>
+        <WebButton>Login</WebButton>
+      </CustomThemeProvider>
       <Login />
     </FormLayout>
   );
