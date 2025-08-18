b209e9c6170035ceef073ee2b207496f2340ec48
Tim Schupp <tim@timschupp.de>
2025-08-18 10:54:17 +0200

    with new error boundary

--- app/login.tsx
+++ app/login.tsx
@@ -3,10 +3,17 @@
 import LittleWorldWebLazy from "@/src/components/blocks/LittleWorldWebLazy";
 import React, { Suspense } from "react";
 
+import { ErrorBoundary } from "@/src/components/ExpoErrorBoundary";
+import { View } from "react-native";
+
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <LittleWorldWebLazy />
-    </Suspense>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>}>
+          <LittleWorldWebLazy />
+        </Suspense>
+      </ErrorBoundary>
+    </View>
   );
 }
