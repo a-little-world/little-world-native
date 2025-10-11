5be85179dc45af90726b73724043a8bfe9986187
Tim Schupp <tim@timschupp.de>
2025-10-11 14:51:50 +0200

    allow hiding dom debug pannel

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@
 import { DomCommunicationProvider } from "@/src/components/blocks/DomCommunicationCore";
 import DomDebugPanel from "@/src/components/blocks/DomDebugPanel";
 import DomWebViewHost from "@/src/components/blocks/DomWebViewHost";
+import { environment } from "@/src/config/environment";
 import { View } from "react-native";
 
 export default function Page() {
@@ -9,7 +10,7 @@ export default function Page() {
     <DomCommunicationProvider>
       <View style={{ flex: 1, height: 800 }}>
         <DomWebViewHost />
-        <DomDebugPanel />
+        {environment.showDebugPanel && <DomDebugPanel />}
       </View>
     </DomCommunicationProvider>
   );
