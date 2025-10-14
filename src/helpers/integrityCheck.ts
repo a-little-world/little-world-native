12b407d8808b1af4f86d60924fab7f399bab079d
Tim Schupp <tim@timschupp.de>
2025-10-14 19:21:46 +0200

    updated simplified integrity check

--- src/helpers/integrityCheck.ts
+++ src/helpers/integrityCheck.ts
@@ -1,11 +1,15 @@
 import { environment } from "../config/environment";
 import * as AppIntegrity from '@expo/app-integrity';
+import { Platform } from 'react-native';
 
-// TODO: android only at the moment
 export async function requestIntegrityCheck() {
+    const requestHash = `native-secret-${Date.now()}-${Math.random()}`;
+    if (Platform.OS === 'web') {
+        return { integrityToken: "bypass", requestHash: requestHash };
+    }
+
     const cloudProjectNumber = environment.googleCloudProjectNumber;
     await AppIntegrity.prepareIntegrityTokenProvider(cloudProjectNumber);
-    const requestHash = `native-secret-${Date.now()}-${Math.random()}`;
     const integrityToken = await AppIntegrity.requestIntegrityCheck(requestHash);
 
     return { integrityToken, requestHash };
