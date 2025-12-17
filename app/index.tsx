263c52ef2904d492e0b5ac318c023c9a63bba5ea
Tim Schupp <tim@timschupp.de>
2025-12-17 13:13:15 +0100

    fix status bar overlap with app

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,22 @@ import { DomCommunicationProvider } from "@/src/components/blocks/DomCommunicati
 import DomDebugPanel from "@/src/components/blocks/DomDebugPanel";
 import DomWebViewHost from "@/src/components/blocks/DomWebViewHost";
 import { environment } from "@/src/config/environment-native";
+import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Page() {
+  const insets = useSafeAreaInsets();
+
   return (
     <DomCommunicationProvider>
-      <View style={{ height: "100%", width: "100%", display: "block" }}>
-        <DomWebViewHost />
-        {environment.showDebugPanel && <DomDebugPanel />}
+      <View style={{ flex: 1, backgroundColor: "#fff" }}>
+        <View style={{ height: insets.top, backgroundColor: "#fff" }} />
+        <StatusBar style="dark" />
+        <View style={{ flex: 1, width: "100%", display: "block" }}>
+          <DomWebViewHost />
+          {environment.showDebugPanel && <DomDebugPanel />}
+        </View>
       </View>
     </DomCommunicationProvider>
   );
