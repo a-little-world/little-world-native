f22f2f9807589ac5e2555485b4ba4000b116ab91
Tim Schupp <tbscode@users.noreply.github.com>
2026-03-09 21:50:49 +0100

    Bump version and build number to 1.0.27

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "little-world-app",
   slug: "little-world-app",
-  version: "1.0.26",
+  version: "1.0.27",
   orientation: "portrait",
   icon: "./src/assets/images/logo-image.png",
   scheme: "little-world-app",
@@ -14,7 +14,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   newArchEnabled: true,
   owner: "little-world",
   ios: {
-    buildNumber: "1.0.26",
+    buildNumber: "1.0.27",
     supportsTablet: true,
     backgroundColor: "#ffffff",
     bitcode: false,
@@ -45,7 +45,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     package: "com.littleworld.littleworldapp",
-    versionCode: 26,
+    versionCode: 27,
     googleServicesFile: environmentNative.googleServiceInfoFileAndroid,
     adaptiveIcon: {
       foregroundImage: "./src/assets/images/logo-image.png",
