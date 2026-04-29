eaa085c13ec8417b8a144455aa2d85e58d7e0dbd
JannisToelle <github.z8zet@simplelogin.com>
2026-04-29 12:45:37 +0200

    revalidate token in authguard

--- src/components/atoms/AuthGuard.tsx
+++ src/components/atoms/AuthGuard.tsx
@@ -1,4 +1,9 @@
-import { apiFetch, refreshAccessTokens } from "@/src/api/helpers";
+import {
+  apiFetch,
+  navigateToLogin,
+  refreshAccessTokens,
+  TokenStatus,
+} from "@/src/api/helpers";
 import { useAuthStore } from "@/src/store/authStore";
 import { useEffect } from "react";
 import useSWR, { mutate } from "swr";
@@ -8,12 +13,25 @@ const IS_AUTHENTICATED_ENDPOINT = "/api/user/authenticated";
 function AuthGuard({ children }: { children: React.ReactNode }) {
   const { data, error, isLoading } = useSWR<boolean>(
     IS_AUTHENTICATED_ENDPOINT,
-    apiFetch,
+    (endpoint) =>
+      apiFetch(endpoint).then(async (isAuthenticated) => {
+        if (!isAuthenticated) {
+          const tokenStatus = await refreshAccessTokens();
+          if (
+            tokenStatus === TokenStatus.EXPIRED ||
+            tokenStatus === TokenStatus.MISSING
+          ) {
+            await navigateToLogin(tokenStatus === TokenStatus.EXPIRED);
+            return false;
+          }
+          return true;
+        }
+        return true;
+      }),
     {
       refreshInterval: (isAuthenticated) => {
         // keep polling every 3s until authenticated
         if (isAuthenticated !== true) {
-          refreshAccessTokens();
           return 3000;
         }
         return 0;
