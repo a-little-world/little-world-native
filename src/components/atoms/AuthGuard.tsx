e3f4721b491b26467dffc56c0ff39682ecbde05e
JannisToelle <github.z8zet@simplelogin.com>
2026-06-08 11:00:34 +0200

    simplify AuthGuard

--- src/components/atoms/AuthGuard.tsx
+++ src/components/atoms/AuthGuard.tsx
@@ -1,33 +1,9 @@
-import {
-  apiFetch,
-  navigateToLogin,
-  refreshAccessTokens,
-  TokenStatus,
-} from "@/src/api/helpers";
-import { useAuthStore } from "@/src/store/authStore";
-import { useEffect } from "react";
-import useSWR, { mutate } from "swr";
-
-const IS_AUTHENTICATED_ENDPOINT = "/api/user/authenticated";
+import { IS_AUTHENTICATED_ENDPOINT } from "@/src/api";
+import useSWR from "swr";
 
 function AuthGuard({ children }: { children: React.ReactNode }) {
   const { data, error, isLoading } = useSWR<boolean>(
     IS_AUTHENTICATED_ENDPOINT,
-    (endpoint) =>
-      apiFetch(endpoint).then(async (isAuthenticated) => {
-        if (!isAuthenticated) {
-          const tokenStatus = await refreshAccessTokens();
-          if (
-            tokenStatus === TokenStatus.EXPIRED ||
-            tokenStatus === TokenStatus.MISSING
-          ) {
-            await navigateToLogin(tokenStatus === TokenStatus.EXPIRED);
-            return false;
-          }
-          return true;
-        }
-        return true;
-      }),
     {
       refreshInterval: (isAuthenticated) => {
         // keep polling every 3s until authenticated
@@ -38,13 +14,6 @@ function AuthGuard({ children }: { children: React.ReactNode }) {
       },
     },
   );
-
-  const { refreshToken } = useAuthStore();
-
-  // check authentication status when tokens change
-  useEffect(() => {
-    mutate(IS_AUTHENTICATED_ENDPOINT);
-  }, [refreshToken]);
 
   const isAuthenticated = data && !isLoading && !error;
   return isAuthenticated ? children : null;
